0722bdec98e7d6ecf0e57f9071347435af4fcaab
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-10-07 16:01:29 +0530

    FoodStore modified

--- src/com/bl/FoodStore.java
+++ src/com/bl/FoodStore.java
@@ -5,8 +5,20 @@ import java.util.List;
 
  //data layer//model layer
 public class FoodStore implements foodMethodInterface{
+	private static FoodStore instance;
 	private List<FoodItem> foodList = new ArrayList();
- 
+	
+	private FoodStore() {
+		
+	}
+	
+	public static synchronized FoodStore getInstance(){
+		if( instance == null) {
+			instance = new FoodStore();
+		}
+		return instance;
+	}
+	
 	public void add(FoodItem foodItem) {
 		foodList.add(foodItem);
 	}
@@ -20,11 +32,13 @@ public class FoodStore implements foodMethodInterface{
 	}
 	
 	public FoodItem getFood (String foodName) {
-		for (FoodItem foodItem : foodList ){
-			if(foodName.equals(foodItem.name)) {
-				return foodItem;
-			}
-		}
-		return null;
+//		for (FoodItem foodItem : foodList ){
+//			if(foodName.equals(foodItem.name)) {
+//				return foodItem;
+//			}
+//		}
+//		return null;
+		return foodList.stream().filter(foodItem -> foodName.equals(foodItem.name)).findFirst().orElse(null);
+
 	} 
 }
